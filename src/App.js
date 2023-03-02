e8f0c78ef34bfdba311d5f1a64d9e48a72cca16e
Emily Johnson <emelizajo22@gmail.com>
2023-03-01 18:56:17 -0800

    optimize and deconstruct this.state

--- src/App.js
+++ src/App.js
@@ -30,11 +30,22 @@ class App extends Component {
     );
   }
 
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase();
+    this.setState(
+      () => {
+      return { searchField };
+    });
+  }
+
   render() {
     console.log('render');
 
-    const filterMonsters = this.state.monsters.filter((monster) => {
-      return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+
+    const filterMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
     });
 
     return (
@@ -43,15 +54,7 @@ class App extends Component {
           className='search-box' 
           type='search' 
           placeholder='search monsters' 
-          onChange={(event) => {
-            const searchField = event.target.value.toLocaleLowerCase();
-
-            this.setState(
-              () => {
-              return { searchField };
-              }
-            );
-          }} 
+          onChange={onSearchChange} 
         />
         {filterMonsters.map((monster) => {
             return (
