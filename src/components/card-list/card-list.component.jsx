1be9e4bedb04dac7a65924e294f9e499f675429a
Emily Johnson <emelizajo22@gmail.com>
2023-03-06 14:24:00 -0800

    add monster source

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,7 +1,7 @@
 import { Component } from 'react';
 
 import './card-list.styles.css';
-import './card.styles.css';
+// import './card.styles.css';
 
 class CardList extends Component {
   render() {
@@ -11,9 +11,10 @@ class CardList extends Component {
     return (
       <div className="card-list">
         {monsters.map(monster => (
-          <div className="card-container">
-
-
+          <div className="card-container" key={monster.id}>
+            <img alt={`monster ${monster.name}`} src={`https://robohash.org/${monster.id}?set=set2&size=180x180`} />
+            <h2>{monster.name}</h2>
+            <p>{monster.email}</p>
           </div>
         ))}
       </div>
