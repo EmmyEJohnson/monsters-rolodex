8232efc3d1f1355e13f7093a77c2873bef8e0e8f
Emily Johnson <emelizajo22@gmail.com>
2023-03-06 14:03:35 -0800

    add search box styles

--- src/App.js
+++ src/App.js
@@ -49,7 +49,7 @@ class App extends Component {
     return (
       <div className="App">
         <SearchBox 
-          className='search-box'
+          className='monsters-search-box'
           onChangeHandler={onSearchChange} 
           placeholder='search monsters' 
         />
