dc4f6b3a532a4bea6f3792ceb036535f272bdb54
Emily Johnson <emelizajo22@gmail.com>
2023-03-06 14:43:23 -0800

    optimize card component

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,7 +1,8 @@
 import { Component } from 'react';
 
+import Card from '../card/card.component';
 import './card-list.styles.css';
-import './card.styles.css';
+
 
 class CardList extends Component {
   render() {
@@ -11,14 +12,11 @@ class CardList extends Component {
     return (
       <div className="card-list">
         {monsters.map(monster => {
-          const { name, email, id } = monster;
+
           return (
-          <div className="card-container" key={id}>
-            <img alt={`monster ${name}`} src={`https://robohash.org/${id}?set=set2&size=180x180`} />
-            <h2>{name}</h2>
-            <p>{email}</p>
-          </div>
-        )})}
+            <Card monster={monster} />
+          );
+        })}
       </div>
     )
   }
