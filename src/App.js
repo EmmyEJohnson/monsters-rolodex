2f05898b7be9498d61fefc31029fa65afc9e86e1
Emily Johnson <emelizajo22@gmail.com>
2023-03-07 09:58:37 -0800

    start converting from class components to functional components

--- src/App.js
+++ src/App.js
@@ -4,66 +4,86 @@ import CardList from './components/card-list/card-list.component';
 import SearchBox from './components/search-box/search-box.component';
 import './App.css';
 
-class App extends Component {
-  constructor() {
-    super();
+const App = () => {
 
-    this.state = {
-      monsters: [],
-      searchField: '',
-    };
-  }
+  return (
+    <div className="App">
+    <h1 className="app-title">Monsters Rolodex</h1>
 
-  componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then((response) => response.json())
-    .then((users) => 
-      this.setState(
-        () => {
-          return { monsters: users}
-        },
-        () => {
-          console.log(this.state);
-        }
-      ) 
-    );
-  }
-
-  onSearchChange = (event) => {
-    const searchField = event.target.value.toLocaleLowerCase();
-    this.setState(
-      () => {
-      return { searchField };
-    });
-  }
-
-  render() {
-
-    const { monsters, searchField } = this.state;
-    const { onSearchChange } = this;
-
-    const filteredMonsters = monsters.filter((monster) => {
-      return monster.name.toLocaleLowerCase().includes(searchField);
-    });
-
-    return (
-      <div className="App">
-        <h1 className="app-title">Monsters Rolodex</h1>
-
-        <SearchBox 
-          className='monsters-search-box'
-          onChangeHandler={onSearchChange} 
-          placeholder='search monsters' 
-        />
-        <CardList 
-          monsters={filteredMonsters} 
-        />
-        
-      </div>
-    );
+    <SearchBox 
+      className='monsters-search-box'
+      onChangeHandler={onSearchChange} 
+      placeholder='search monsters' 
+    />
+    <CardList 
+      monsters={filteredMonsters} 
+    />
     
-  }
-  
+  </div>
+  )
 }
 
+// class App extends Component {
+//   constructor() {
+//     super();
+
+//     this.state = {
+//       monsters: [],
+//       searchField: '',
+//     };
+//   }
+
+//   componentDidMount() {
+//     fetch('https://jsonplaceholder.typicode.com/users')
+//       .then((response) => response.json())
+//       .then((users) => 
+//         this.setState(() => {
+//           return { monsters: users}
+//         },
+//         () => {
+//           console.log(this.state);
+//         }
+//       ) 
+//     );
+//   }
+
+//   onSearchChange = (event) => {
+//     const searchField = event.target.value.toLocaleLowerCase();
+//     this.setState(
+//       () => {
+//       return { searchField };
+//     });
+//   }
+
+//   render() {
+
+//     const { monsters, searchField } = this.state;
+//     const { onSearchChange } = this;
+
+//     const filteredMonsters = monsters.filter((monster) => {
+//       return monster.name.toLocaleLowerCase().includes(searchField);
+//     });
+
+//     return (
+//       <div className="App">
+//         <h1 className="app-title">Monsters Rolodex</h1>
+
+//         <SearchBox 
+//           className='monsters-search-box'
+//           onChangeHandler={onSearchChange} 
+//           placeholder='search monsters' 
+//         />
+//         <CardList 
+//           monsters={filteredMonsters} 
+//         />
+        
+//       </div>
+//     );
+    
+//   }
+  
+// }
+
 export default App;
+
+// When converting to functional components you don't need constructors, life cycle methods, and render. They are only unique to class components.
